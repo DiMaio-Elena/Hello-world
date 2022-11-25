ded7dc627f63a3bbed58dc97f350fc4520cd8b5f
DiMaio-Elena <di.maio.elena@iisgalvanimi.edu.it>
2022-11-25 10:48:35 +0000

    v.3/home/gitpod/.pyenv/shims/python

--- Preparazione Verifica/Punto 4.py
+++ Preparazione Verifica/Punto 4.py
@@ -1,10 +1,11 @@
 
 import random
 casuale = []
-
+valore=0
 
 for i in range (50):
     casuale.append(random.randint(0,100))
 for x in casuale :
     if x>50 or x<10 :
         print(x)
+valore=valore+1
